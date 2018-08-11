efdd2785b2fad78b2fe2182fd32162b5cc456c52
yeeeeerin <hepari2154@naver.com>
2018-08-11 14:34:45 +0900

    [8/11] 토비의 스프링 v1 - 2장 완료

--- chapter1/src/test/java/com/example/chapter1/JUnitTest.java
+++ chapter1/src/test/java/com/example/chapter1/JUnitTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
@@ -14,23 +17,23 @@ import static org.hamcrest.Matchers.is;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObject = new HashSet<JUnitTest>();
 
     @Test
     public  void test1(){
         assertThat(this,is(not(sameInstance(testObject))));
-        testObject = this;
+        testObject.add(this);
     }
 
     @Test
     public  void test2(){
         assertThat(this,is(not(sameInstance(testObject))));
-        testObject = this;
+        testObject.add(this);
     }
 
     @Test
     public  void test3(){
         assertThat(this,is(not(sameInstance(testObject))));
-        testObject = this;
+        testObject.add(this);
     }
 }
