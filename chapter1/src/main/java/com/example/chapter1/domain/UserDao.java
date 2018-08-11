5e576b53abe84251d539aaf322935e59bcdf7780
yeeeeerin <hepari2154@naver.com>
2018-08-11 16:01:04 +0900

    [8/11]토비의 스프링 v1 - 3.3.1

--- chapter1/src/main/java/com/example/chapter1/domain/UserDao.java
+++ chapter1/src/main/java/com/example/chapter1/domain/UserDao.java
@@ -1,5 +1,6 @@
 package com.example.chapter1.domain;
 
+import com.example.chapter1.dao.AddStatement;
 import com.example.chapter1.dao.DeleteAllStatement;
 import com.example.chapter1.dao.StatementStrategy;
 import lombok.Setter;
@@ -16,18 +17,8 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException,SQLException{
 
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) value (?,?,?)");
-
-        ps.setString(1,user.getId());
-        ps.setString(2,user.getName());
-        ps.setString(3,user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
