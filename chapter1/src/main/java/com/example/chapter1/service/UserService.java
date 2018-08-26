2d3dbf66980eb3682224c41b797238695dea5824
yerin <hepari2154@naver.com>
2018-08-26 22:20:38 +0800

    8/26]토비의 스프링 v1 - 5.4.1(java mail을 이용한 발송 메소드 추가)

--- chapter1/src/main/java/com/example/chapter1/service/UserService.java
+++ chapter1/src/main/java/com/example/chapter1/service/UserService.java
@@ -1,22 +1,26 @@
 package com.example.chapter1.service;
 
 import com.example.chapter1.dao.UserDao;
-import com.example.chapter1.dao.UserLevelUpgradePolicy;
 import com.example.chapter1.domain.Level;
 import com.example.chapter1.domain.User;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
 @Getter
 @Setter
@@ -57,6 +61,36 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
+
+    }
+
+    /*
+    업그레이드되는 사용자에게 안내메일을 발송하는 기능
+     */
+    private void sendUpgradeEmail(User user){
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host","mail.lyr.org");
+        Session s = Session.getInstance(properties,null);
+
+        MimeMessage message = new MimeMessage(s);
+
+        try {
+            message.setFrom(new InternetAddress("useradmin@lyl.org"));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이" + user.getLevel().name() + "로 업그레이드 되었습니다");
+
+            Transport.send(message);
+
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public boolean canUpgradeLevel(User user){
