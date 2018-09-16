f50c089efede5c507333c42fb50ad8d0e1e0de4c
yerin <hepari2154@naver.com>
2018-09-16 16:22:56 +0800

    [9/16]토비의 스프링 v2 시작

---
+++ vol2/src/main/java/com/example/vol2/Vol2Application.java
@@ -0,0 +1,12 @@
+package com.example.vol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vol2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vol2Application.class, args);
+    }
+}
