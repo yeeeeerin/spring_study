03efa8941a0ae08f49468744e60c6a8ecf5d3694
yerin <hepari2154@naver.com>
2019-02-25 02:02:15 +0900

    [19/02/25] aop project init

---
+++ AopProject/src/main/java/com/example/aopproject/AopprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.aopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopprojectApplication.class, args);
+    }
+
+}
